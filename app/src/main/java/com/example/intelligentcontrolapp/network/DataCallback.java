3db2bc02d96008eb74bff2be9edec6801f6c3fc4
wzy <2049562063@qq.com>
2024-11-03 14:30:13 +0800

    remove dup file

--- app/src/main/java/com/example/intelligentcontrolapp/network/DataCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.intelligentcontrolapp.network;
-
-import org.json.JSONObject;
-
-public interface DataCallback {
-    void onSuccess(JSONObject DataInfo);
-    void onError(String errorMessage);
-}
--- app/src/main/java/com/example/intelligentcontrolapp/network/LoginCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.intelligentcontrolapp.network;
-
-public interface LoginCallback {
-    void onSuccess(String token);
-
-    void onError(String error);
-}
